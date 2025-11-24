8baf2215e76d0bc9484e28a0901a0ea5fcd1de39
hyun1217 <psh_1217@naver.com>
2025-11-24 18:34:23 +0900

    11

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,13 +1,13 @@
 import {createClient} from "@supabase/supabase-js";
 
-const url = "https://smhvcspigkhonzmznkxb.supabase.co";
-const anon_key =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtaHZjc3BpZ2tob256bXpua3hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTE0NzcsImV4cCI6MjA3ODY2NzQ3N30.9N_lq3a52jfWclqVG2eMMbonxj3XQaXqkSXz6sOVriM";
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// const url = "https://smhvcspigkhonzmznkxb.supabase.co";
+// const anon_key =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtaHZjc3BpZ2tob256bXpua3hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwOTE0NzcsImV4cCI6MjA3ODY2NzQ3N30.9N_lq3a52jfWclqVG2eMMbonxj3XQaXqkSXz6sOVriM";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = url;
-const supabaseKey = anon_key;
+// const supabaseUrl = url;
+// const supabaseKey = anon_key;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
