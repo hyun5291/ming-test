bc32ce4f79550d01ebc0f34a9624a1b33ffa98b0
hyun1217 <psh_1217@naver.com>
2025-11-26 19:04:37 +0900

    123

--- src/components/topic/new-topic.tsx
+++ src/components/topic/new-topic.tsx
@@ -1,7 +1,7 @@
 import {UserInfo} from "./user-info";
 import {Card, Separator} from "../ui";
 import {CaseSensitive, ChartNoAxesColumnIncreasing, Heart, MessageCircleMore} from "lucide-react";
-import ElectricBorder from "../ui/ElectricBorder";
+// import ElectricBorder from "../ui/ElectricBorder";
 import {useNavigate} from "react-router";
 import type {Topic} from "@/types";
 
@@ -37,55 +37,55 @@ function extractTextfromContent(content?: string, maxChars = 100) {
 function NewTopic({props, color}: Props) {
     const navigate = useNavigate();
     return (
-        <ElectricBorder color={color} speed={1} chaos={0.2} thickness={2} style={{borderRadius: 16}} className="min-w-[520px] max-w-[520px]">
-            <div className="p-3 opacity-80">
-                <Card className="p-4 gap-4" onClick={() => navigate(`/topic/${props ? props?.id : "user"}`)}>
-                    <div className="h-fit flex items-center justify-between gap-4">
-                        <div className="h-full flex flex-col justify-between">
-                            {/* 제목 */}
-                            <div className="flex flex-col">
-                                <CaseSensitive size={16} className="text-neutral-500" />
-                                <p className="font-semibold text-base line-clamp-2">
-                                    {props ? props?.title : "NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다."}
-                                </p>
-                            </div>
-                            {/* 본문 */}
-                            <p className="text-neutral-500 line-clamp-3">
-                                {props
-                                    ? extractTextfromContent(props?.content)
-                                    : "NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다."}
+        // <ElectricBorder color={color} speed={1} chaos={0.2} thickness={2} style={{borderRadius: 16}} className="min-w-[520px] max-w-[520px]">
+        <div className="p-3 opacity-80 min-w-[520px] max-w-[520px]">
+            <Card className="p-4 gap-4" onClick={() => navigate(`/topic/${props ? props?.id : "user"}`)}>
+                <div className="h-fit flex items-center justify-between gap-4">
+                    <div className="h-full flex flex-col justify-between">
+                        {/* 제목 */}
+                        <div className="flex flex-col">
+                            <CaseSensitive size={16} className="text-neutral-500" />
+                            <p className="font-semibold text-base line-clamp-2">
+                                {props ? props?.title : "NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다.NEW Topic 제목 문구입니다."}
                             </p>
                         </div>
-                        {/* 썸넬 */}
-                        <div className="w-35 min-w-35 h-35 bg-accent rounded-md">
-                            {/* <img src="/vite.svg" alt="@SAMPLE_IMAGE" className="w-35 min-w-35 bg-accent rounded-md" /> */}
-                            <img src={props ? props?.thumbnail : "/vite.svg"} alt="@SAMPLE_IMAGE" className="w-full h-full bg-accent rounded-md object-cover" />
-                        </div>
+                        {/* 본문 */}
+                        <p className="text-neutral-500 line-clamp-3">
+                            {props
+                                ? extractTextfromContent(props?.content)
+                                : "NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다.NEW Topic 컨텐츠 문구입니다."}
+                        </p>
                     </div>
-                    <Separator />
-                    <div className="flex items-end justify-between">
-                        <UserInfo uid={props?.author} />
+                    {/* 썸넬 */}
+                    <div className="w-35 min-w-35 h-35 bg-accent rounded-md">
+                        {/* <img src="/vite.svg" alt="@SAMPLE_IMAGE" className="w-35 min-w-35 bg-accent rounded-md" /> */}
+                        <img src={props ? props?.thumbnail : "/vite.svg"} alt="@SAMPLE_IMAGE" className="w-full h-full bg-accent rounded-md object-cover" />
+                    </div>
+                </div>
+                <Separator />
+                <div className="flex items-end justify-between">
+                    <UserInfo uid={props?.author} />
+                    <div className="flex items-center gap-3">
                         <div className="flex items-center gap-3">
-                            <div className="flex items-center gap-3">
-                                <div className="flex items-center gap-1">
-                                    <ChartNoAxesColumnIncreasing size={14} />
-                                    <p>{props?.viewCounts === null ? 0 : props?.viewCounts}</p>
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <MessageCircleMore size={14} />
-                                    <p>{props?.commentCounts === null ? 0 : props?.commentCounts}</p>
-                                </div>
-                            </div>
-                            <Separator orientation="vertical" className="h-3!" />
                             <div className="flex items-center gap-1">
-                                <Heart size={14} className="text-rose-500" />
-                                <p>{props?.likeCounts === null ? 0 : props?.likeCounts}</p>
+                                <ChartNoAxesColumnIncreasing size={14} />
+                                <p>{props?.viewCounts === null ? 0 : props?.viewCounts}</p>
+                            </div>
+                            <div className="flex items-center gap-1">
+                                <MessageCircleMore size={14} />
+                                <p>{props?.commentCounts === null ? 0 : props?.commentCounts}</p>
                             </div>
                         </div>
+                        <Separator orientation="vertical" className="h-3!" />
+                        <div className="flex items-center gap-1">
+                            <Heart size={14} className="text-rose-500" />
+                            <p>{props?.likeCounts === null ? 0 : props?.likeCounts}</p>
+                        </div>
                     </div>
-                </Card>
-            </div>
-        </ElectricBorder>
+                </div>
+            </Card>
+        </div>
+        // </ElectricBorder>
     );
 }
 
